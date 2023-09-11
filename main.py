055bcfd5be0e0a879f62339bbe0d5b6cf5bbe6fd
FFlores0914 <fflores0914@outlook.com>
2023-09-11 08:42:12 -0600

    se agrego La operación de borrar la terminal

--- main.py
+++ main.py
@@ -13,3 +13,20 @@ while True:
 
     print(f"Presionaste la tecla: {tecla}")
 
+import os
+
+def borrar_terminal():
+    # Esta función borra la terminal según el sistema operativo
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+    numero = 0
+
+    while numero <= 50:
+        borrar_terminal()
+        print(f"Número actual: {numero}")
+        input("Presiona 'n' y Enter para continuar...")
+        numero += 1
+if __name__ == "__main__": 
+    main()
+
